d6625987d32937b2444b9ab71e227f84d3c30326
Djboy08 <justindoane@gmail.com>
2023-04-17 19:03:05 -0400

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compress: false,
   experimental: {
     appDir: true,
+    mdxRs: true,
   },
 }
 
-module.exports = nextConfig
+
+export default nextConfig
